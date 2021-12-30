55f34df5b62ec9fad401eeafcbc9309fe5ebacf9
Melchisedek Hengue-Touomo <melchisedek.hengue-touomo@vrm.de>
2021-12-30 17:17:54 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,13 @@
 <template>
   <div id="container">
-    <Users />
-    <Count />
   </div>
 
 </template>
 
 <script>
-import Users from 'Components/users/Users.vue';
-import Count from 'Components/Count.vue';
-
 export default {
   name: 'app',
   components: {
-    Users,
-    Count
   }
 }
 </script>
